beb608c9f0ab753c5d38a2122cd7310f19f295b7
vrkhans <vrkhans@gmail.com>
2016-08-11 17:10:05 -0400

    first commit

---
+++ app/ts/app.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {ChildOneComponent} from './child-One.component';
+import {PairComponent} from './pair.component';
+import {Pair}from './Pair';
+
+@Component({
+    selector: 'my-app',
+    template: ` <div>This is the Root App Component</div>
+                <child-one data = "data sent to public property"></child-one>
+                <hr>
+                
+                <pair *ngFor="let mypair of pairs" [nameValue]="mypair"></pair>
+              `,
+    directives: [ChildOneComponent,PairComponent]
+})
+export class AppComponent { 
+pairs: Pair[];
+    constructor() {
+        this.pairs = [
+            new Pair("cotton", 6),
+            new Pair("food", 9)
+        ]
+    }
+}
